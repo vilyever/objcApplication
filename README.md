81c57d6236e9caf4226b3b7f23afd9ca2d3978ab
vilyever <250450035@163.com>
2016-08-05 14:25:52 +0800

    first release

---
+++ README.md
@@ -0,0 +1,17 @@
+# objcApplication
+objcApplication
+
+## Import
+### [CocoaPods](http://cocoapods.org)
+use_frameworks!
+</br>
+pod 'objcApplication'
+
+### [Carthage](https://github.com/Carthage/Carthage)
+github "vilyever/objcApplication" "master"
+
+## Usage
+
+## License
+
+[MIT license](LICENSE)
--- objcApplication/objcApplication.h
+++ objcApplication/objcApplication.h
@@ -16,4 +16,7 @@ FOUNDATION_EXPORT const unsigned char objcApplicationVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <objcApplication/PublicHeader.h>
 
-
+#if !VDSharedApplication
+#define VDSharedApplication \
+[UIApplication sharedApplication]
+#endif
