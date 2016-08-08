eec38ef6abde482e7d01c1681619c5a5ab93212c
vilyever <250450035@163.com>
2016-08-08 14:46:55 +0800

    fix project

--- objcApplication/objcApplication.h
+++ objcApplication/objcApplication.h
@@ -16,6 +16,9 @@ FOUNDATION_EXPORT const unsigned char objcApplicationVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <objcApplication/PublicHeader.h>
 
+#import <objcApplication/UIApplication+VDNotification.h>
+#import <objcApplication/UIApplication+VDStatusBar.h>
+
 #if !VDSharedApplication
 #define VDSharedApplication \
 [UIApplication sharedApplication]
